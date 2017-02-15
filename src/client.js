9503cf5ccf8ee941e7a6da3fe8af65be2e0996c1
restrry <restrry@gmail.com>
2017-02-15 11:24:48 +0100

    demo error react-modal

--- src/client.js
+++ src/client.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from 'react-modal';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
-import configureStore from './store/';
-import './polyfills/';
-import App from './components/app/';
+class Wrapper extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: false
+        };
+    }
+    unstable_handleError(e) {
+        this.setState({ error: true });
+    }
+    render() {
+        if (this.state.error) {
+            return <div>error</div>;
+        }
 
-const store = configureStore();
+        return (
+            <div>
+                { this.props.children }
+            </div>
+        );
+    }
+}
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+const BadComponent = () => {
+    throw new Error('oops');
+    return <div>Some markup</div>
+};
+
+const App = () => (
+    <Wrapper>
+        <Modal isOpen={true} title="title">
+            modal body
+        </Modal>
+        <BadComponent />
+    </Wrapper>
 );
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<App />, document.querySelector('#root'));
+}, false);
+
